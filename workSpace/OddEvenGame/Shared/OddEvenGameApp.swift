212978cc15156b3ed1becc10b002e8a3783f6252
seongjoocho <seongjooc@gmail.com>
2022-03-01 22:35:45 +0900

    create new project

---
+++ workSpace/OddEvenGame/Shared/OddEvenGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddEvenGameApp.swift
+//  Shared
+//
+//  Created by 성주の on 2022/03/01.
+//
+
+import SwiftUI
+
+@main
+struct OddEvenGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
