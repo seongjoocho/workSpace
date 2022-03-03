f743c0d849bfbb1d499aa851fb0e1fba816cd560
seongjoocho <seongjooc@gmail.com>
2022-03-03 13:10:08 +0900

    홀짝 로직 개발

--- workSpace/OddOrEvenGame/OddOrEvenGame/ViewController.swift
+++ workSpace/OddOrEvenGame/OddOrEvenGame/ViewController.swift
@@ -4,6 +4,12 @@
 //
 //  Created by 성주の on 2022/03/01.
 //
+/*
+ 1. 컴퓨터가 1에서 10까지의 랜덤으로 숫자를 선택.
+ 2. 사용자는 가진 구슬 개수를 걸고 홀/짝 중 하나를 선택.
+ 3. 결과값이 화면에 보여진다.
+ */
+
 
 import UIKit
 
@@ -11,6 +17,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var computerBallCountLbl: UILabel!
     @IBOutlet weak var userBallCountLbl: UILabel!
+    @IBOutlet weak var resultLbl: UILabel!
     
     var comBallsCout: Int = 20
     var userBallsCount: Int = 20
@@ -26,6 +33,79 @@ class ViewController: UIViewController {
 
     @IBAction func gameStarPressed(_ sender: Any) {
         print("게임시작 !")
+        print(self.getRandom())
+        
+        let alert = UIAlertController.init(title: "Game Start", message: "홀 짝을 선택해주세요.", preferredStyle: .alert)
+        
+        let oddBtn = UIAlertAction.init(title: "홀", style: .default) { _ in
+            print("홀 버튼을 클릭했습니다.")
+            
+            guard let input = alert.textFields?.first?.text, let value = Int(input) else {
+                return
+            }
+            
+            self.getWinner(count: value, select: "홀")
+            }
+        
+        let evenBtn = UIAlertAction.init(title: "짝", style: .default) { _ in
+            print("짝 버튼을 클릭했습니다.")
+            
+            guard let input = alert.textFields?.first?.text else {
+                return
+        }
+            
+                guard let value = Int(input) else {
+                    return
+            }
+            
+            self.getWinner(count: value, select: "짝")
+            
+        }
+        
+        alert.addTextField { textField in
+            textField.placeholder = "베팅 할 구슬의 개수를 입력해주세요."
+        }
+        
+        alert.addAction(oddBtn)
+        alert.addAction(evenBtn)
+        
+        self.present(alert, animated: true) {
+            print("화면이 띄워졌습니다.")
+        }
+    }
+    func getWinner(count: Int, select: String) {
+        let com = self.getRandom()
+        let comType = com % 2 == 0 ? "짝" : "홀"
+        
+        
+        var result = comType
+        if comType == select {
+            print("User win")
+            result = result + "(User Win)"
+            self.calculateBalls(winner: "user", count: count)
+        }else{
+            result = result + "(Com Win)"
+            print("Computer win")
+            self.calculateBalls(winner: "com", count: count)
+        }
+        self.resultLbl.text = result
+    }
+    func calculateBalls(winner: String, count: Int){
+        
+        if winner == "com" {
+            self.userBallsCount = self.userBallsCount - count
+            self.comBallsCout = self.comBallsCout + count
+        }else{
+            self.userBallsCount = self.userBallsCount + count
+            self.comBallsCout = self.comBallsCout - count
+        }
+        self.userBallCountLbl.text = "\(self.userBallsCount)"
+        self.computerBallCountLbl.text = "\(self.comBallsCout)"
+        
+    }
+    
+    func getRandom() -> Int {
+        return Int(arc4random_uniform(10) + 1)
     }
     
 }
